4c5482c9dbe207fb9558f55773f8f3d4069b4429
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-04-11 19:28:42 +0200

    crea un lazy-loader

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-loader',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent {
+
+}
